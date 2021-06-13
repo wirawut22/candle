553db675a38a3c8f0facdcc26b486a396c5af0fc
wirawut <wirawut22@gmail.com>
2021-06-14 01:04:33 +0700

    initial

---
+++ components/pages/layout/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a
+          class="nav-link"
+          data-widget="pushmenu"
+          href="#"
+        ><i class="fas fa-bars" /></a>
+      </li> 
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false">
+          <i class="far fa-user"></i> {{user?user.name:""}}
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
+          <a href="#" class="dropdown-item">
+            <div class="media"> 
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  ผู้ใช้งาน : {{user?user.name:""}}
+                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">อีเมล์ : {{user?user.email:""}}</p> 
+              </div>
+            </div> 
+          </a> 
+          <div class="dropdown-divider"></div> 
+        </div>
+      </li>  -->
+      <li class="nav-item"> 
+        <a class="nav-link" @click="logout"> 
+          <i class="fas fa-sign-out-alt" title="ออกจากระบบ"/>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+</template>
+
+<style scoped>
+  .nav-link{
+    cursor:pointer;
+  }
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      user: this.$auth.user,
+      loggedIn: this.$auth.loggedIn
+    };
+  },
+  methods: {
+    async logout() {
+      await this.$auth.logout();
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
---
+++ pages/login/index.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="hold-transition login-page"> 
+    <div id="loading-login-modal" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog modal-dialog-centered justify-content-center" role="document">
+            <span class="fas fa-cog fa-spin fa-3x"></span>
+        </div>
+    </div> 
+
+    <div class="login-box">
+      <!-- <div class="login-logo">
+        <b>ผู้ดูเเลระบบ</b>
+      </div> -->
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">ล็อกอินเพื่อจัดการข้อมูล</p>
+
+          <form @submit="login"> 
+            <div class="input-group mb-3">
+              <label v-if="message=='invalid'" class="col-form-label" style="color:red;" for="inputError"><i class="far fa-times-circle"></i> ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง</label>
+            </div>
+            <div class="input-group mb-3">
+              <input type="text" v-model="username" class="form-control" placeholder="ชื่อผู้ใช้งาน" />
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-user"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="password" v-model="password" class="form-control" placeholder="รหัสผ่าน" />
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-7">
+                <div class="icheck-primary">
+                  <input type="checkbox" id="remember" />
+                  <label for="remember"> จดจำรหัสผ่าน </label>
+                </div>
+              </div>
+              <!-- /.col -->
+              <div class="col-5">
+                <button type="submit" class="btn btn-info btn-block">
+                  <i class="fas fa-sign-in-alt"></i> เข้าสู่ระบบ
+                </button>
+              </div>
+              <!-- /.col -->
+            </div>
+          </form>
+ 
+
+          <p class="mb-1">
+            <a href="forgot-password.html">ลืมรหัสผ่าน</a>
+          </p> 
+        </div>
+        <!-- /.login-card-body -->
+      </div>
+    </div>
+    <!-- /.login-box -->
+  </div>
+</template>
+
+<style scoped>
+  #loading-login-modal .fa, #loading-login-modal .fas { 
+    color: #ffffff;
+  }
+</style>
+
+
+<script>
+export default {
+  layout: "coreLayout",
+  data() {  
+    debugger;
+    return {
+      message:"",
+      username: '',
+      password: '' 
+    };
+  }, 
+  methods: {
+    async login(e) { 
+      e.preventDefault();
+
+      //$('#loading-modal').modal('show');  
+      
+      const payload = {
+        username: this.username,
+        password: this.password
+      };
+      try {
+        await this.$auth.loginWith('local', {
+          data: payload
+        });
+        //$('#loading-modal').modal('hide') 
+        this.message = 'valid';
+        this.$router.push('/admin/news'); 
+      } catch (e) { 
+        //$('#loading-modal').modal('hide')
+        this.message = 'invalid';
+        //this.$router.push('/login');
+      }
+    }
+  }
+};
+</script>
